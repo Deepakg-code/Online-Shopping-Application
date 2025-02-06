24d185790758ca813276ede7c3b4f9493eb002b3
Deepak <deepakadhi0808@gmail.com>
2025-02-06 16:46:04 +0530

    creating response structure and restrespoonse builder class to send  more convinent resposne to the  user end

---
+++ src/main/java/com/jsp/osa/utility/RestResponseBuilder.java
@@ -0,0 +1,20 @@
+package com.jsp.osa.utility;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestResponseBuilder {
+
+    public <T> ResponseEntity<ResponseStructure<T>> success(HttpStatus status, String message, T data){
+        return ResponseEntity
+                .status(status)
+                .body(ResponseStructure.<T>builder()
+                        .status(status.value())
+                        .message(message)
+                        .data(data)
+                        .build());
+    }
+
+}
