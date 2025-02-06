b3d61f3fea83b236b9f750f6b63732f5ba8bc29c
sahilnaiktutorial <naiksahil478@gmail.com>
2025-02-06 16:16:08 +0530

    created  Product entity

--- src/main/java/com/jsp/osa/entity/Product.java
+++ src/main/java/com/jsp/osa/entity/Product.java
@@ -1,4 +1,30 @@
 package com.jsp.osa.entity;
 
+import com.jsp.osa.enums.AvailabilityStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    private String productName;
+    private String productDescription;
+    private  double productPrice;
+    private  int productQuantity;
+    private AvailabilityStatus availabilityStatus;
+
 }
