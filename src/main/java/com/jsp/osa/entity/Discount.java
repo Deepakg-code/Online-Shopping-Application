95a5d01d8fdc4c3d2f2e9ee5e6f46f338448fc0c
sahilnaiktutorial <naiksahil478@gmail.com>
2025-02-06 16:09:38 +0530

    created Discount entity

--- src/main/java/com/jsp/osa/entity/Discount.java
+++ src/main/java/com/jsp/osa/entity/Discount.java
@@ -1,4 +1,27 @@
 package com.jsp.osa.entity;
 
+import com.jsp.osa.enums.DiscountType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int discountId;
+
+    private DiscountType discountType;
+
+    private int discountValue;
+
 }
