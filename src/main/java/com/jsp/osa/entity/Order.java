e80c2cebf00d2ee0564df8661346a58b19da598a
sahilnaiktutorial <naiksahil478@gmail.com>
2025-02-06 16:15:54 +0530

    created  Order entity

--- src/main/java/com/jsp/osa/entity/Order.java
+++ src/main/java/com/jsp/osa/entity/Order.java
@@ -1,4 +1,27 @@
 package com.jsp.osa.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+
+    private  int selectedQuantity;
+    private double totalPrice;
+    private double discountPrice;
+    private double totalPayableAmount;
 }
