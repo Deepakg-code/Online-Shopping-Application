986f5c31a7b33866afab0d38883110f19b297f7d
sahilnaiktutorial <naiksahil478@gmail.com>
2025-02-06 15:28:13 +0530

    created Contact entity

--- src/main/java/com/jsp/osa/entity/Contact.java
+++ src/main/java/com/jsp/osa/entity/Contact.java
@@ -1,4 +1,26 @@
 package com.jsp.osa.entity;
 
+import com.jsp.osa.enums.Priority;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int contactId;
+
+    private String contactNumber;
+    private Priority priority;
 }
